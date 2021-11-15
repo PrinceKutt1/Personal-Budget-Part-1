a05a9b87953bcef8d1f80ddbee8be46247e45dbc
PrinceKutt1 <prince.kutorglo@gmail.com>
2021-11-16 04:00:53 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//Initializing dependencies
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+
+const envelopeRoutes = require('./routes/envelope.js');
+
+//const router = require('./routes/envelope.js');
+
+app.use(bodyparser.json());
+
+app.use('/envelope', envelopeRoutes);
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
